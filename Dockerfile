31b81d25d590f95b0d3cceab0c3b621b853fefbf
Marius Octavin Matei <marius@digitalcareplanning.com>
2019-11-15 17:56:17 +0000

    Added entry script to container

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ ARG BUILDER_DOCKER=swift:$SWIFT_VERSION
 FROM $BUILDER_DOCKER as builder
 
 RUN apt-get -qq update && apt-get -q -y install \
-  libssl-dev libicu-dev
+  libssl-dev \
+  libicu-dev \
+  zip
 
 ADD assets /assets
+ADD build.sh /usr/local/bin/
+
+ENTRYPOINT ["/usr/local/bin/build.sh"]
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# build and pack a swift library
+
+set -eo pipefail
+
+CONFIGURATION=${CONFIGURATION:-release}
+BUILD_FOLDER=${BUILD_FOLDER:-.build-serverless}
+ARTIFACT_FOLDER=${ARTIFACT_FOLDER:-.serverless-swift}
+
+ARTIFACT_LAMBDA_FOLDER=${ARTIFACT_LAMBDA_FOLDER:-lambda}
+ARTIFACT_LAYER_FOLDER=${ARTIFACT_LAYER_FOLDER:-layer}
+
+mkdir -p .${ARTIFACT_FOLDER}/${ARTIFACT_LAMBDA_FOLDER}
+mkdir -p .${ARTIFACT_FOLDER}/${ARTIFACT_LAYER_FOLDER}
+
+swift build --configuration ${CONFIGURATION} --build-path ${BUILD_FOLDER}
+
+find ${BUILD_FOLDER}/${CONFIGURATION}/ -type f -executable -exec cp {} ${PWD}/${ARTIFACT_FOLDER}/${ARTIFACT_LAMBDA_FOLDER}/ \;
+find ${ARTIFACT_FOLDER}/${ARTIFACT_LAMBDA_FOLDER}/ -executable -exec zip -j '{}'.zip '{}' \;
+find ${ARTIFACT_FOLDER}/${ARTIFACT_LAMBDA_FOLDER}/ -executable -exec rm '{}' \;
+
+
